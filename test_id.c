513aa8eca39abb42066fe9e93280e1b3c51fe511
aa <a@a.ru>
2017-03-06 02:33:00 +0300

    Тестовая программа для нахождения имен переменных в программе

--- test_id.c
+++ test_id.c
@@ -6,6 +6,7 @@
 int is_type(char* str);
 int is_space(char ch);
 void skipspace(char* buffer, int* index);
+int in(Sequential* cont, char* str);
 
 int main(int argc, char* argv[]){
 
@@ -17,7 +18,7 @@ int main(int argc, char* argv[]){
 	}
 	else {
 		fin = (char*)malloc(sizeof(char)*11);
-		strcpy(fin, "input.txt");
+		strcpy(fin, "test_id.c");
 	}
 
 	FILE* file = fopen(fin, "r");
@@ -32,13 +33,11 @@ int main(int argc, char* argv[]){
 		perror("fread");
 	printf("%d\t %p\n", size, buffer);
 
-	Sequential* c = list_create(1, (void**)"~~", sizeof(char*));
-	Iterator ptr = c->iterator_init (c);
-	//c->end(c, ptr);
-	//c->set(c, ptr, "~~");
+	Sequential* c = list_create(0, (void**)NULL, sizeof(char*));
+	Iterator ptr = c->iterator_init(c);
+	c->end(c, ptr);
+	c->set(c, ptr, "~~");
 	printf("%p\n", c->get(c, ptr));
-	//char* fff = (char*)c->get(c, ptr);
-	//printf("%s\n", (char*)(c->get(c, ptr)));
 	int index = 0;
 	int i = 0;
 	int k = 0;
@@ -48,30 +47,32 @@ int main(int argc, char* argv[]){
 //START OF PARSING
 //////////////////////////////////////////////////
 	while (index < size){
-		//printf("%c", buffer[index]);
 		if (is_space(buffer[index])){
-			//printf("len %d\n", len);
-			char* str = (char*)malloc(sizeof(char)* (len+1));
+			char* str = (char*)malloc(sizeof(char)*(len+1));
 			k = 0;
+
 			for(i = index - len; i < index; i++)
 				str[k++] = buffer[i];
 			str[k] = 0;
-			if (!strcmp(str, "=")){
+
+			if (!strcmp(str, "="))
 				while((buffer[index] != ';') && (buffer[index] != ','))
 					index++;
-			}
+
 			if (is_good){
-				c->end(c, ptr);
-				c->insert(c, ptr, str);	
-				//printf("%p\n", c->get(c, ptr));
-				is_good = 0;
+				if (!in(c, str)){
+					c->end(c, ptr);
+					c->insert(c, ptr, str);	
+					is_good = 0;
+				}	
+				else
+					is_good = 0;
 			}
 			else if (is_type(str)){
 				is_good = 1;
-				//printf("str %s\n", str);
 			}
-			else
-				free(str);
+			else free(str);
+
 			len = 0;
 			skipspace(buffer, &index);
 		}
@@ -79,22 +80,12 @@ int main(int argc, char* argv[]){
 			len++;
 			index++;
 		}
-		//
 	}
 //////////////////////////////////////////////////
 //END OF PARSING
 //////////////////////////////////////////////////
 
-//////////////////////////////////////////////////
-//START OF DISTINCT
-//////////////////////////////////////////////////
-
-
-
-//////////////////////////////////////////////////
-//END OF DISTINCT
-//////////////////////////////////////////////////
-
+	FILE* fout = fopen("output.txt","w");
 
 	Iterator sptr = c->iterator_init(c);
 	c->begin(c, sptr);
@@ -103,10 +94,10 @@ int main(int argc, char* argv[]){
 	printf("IM HERE\n");
 	c->next(c, sptr);
 	while(*sptr != *ptr){
-		printf("%s\n", (char*)(c->get(c, sptr)));
+		fprintf(fout, "%s\n", (char*)(c->get(c, sptr)));
 		c->next(c, sptr);
 	}
-	printf("%s\n", (char*)(c->get(c, sptr)));
+	fprintf(fout, "%s\n", (char*)(c->get(c, sptr)));
 	
 //////////////////////////////////////////////////
 //CLEANING
@@ -140,4 +131,24 @@ int is_type(char* str){
 
 	return (!strcmp(str, "int") || !strcmp(str, "char")|| !strcmp(str, "void") || !strcmp(str, "float") || !strcmp(str, "double") || !strcmp(str, "struct") );
 
+}
+
+int in(Sequential* cont, char* str){
+
+	int match = 0;
+	Iterator ptr = cont->iterator_init(cont);
+	Iterator fin = cont->iterator_init(cont);
+
+	cont->begin(cont, ptr);
+	cont->end(cont, fin);
+	while(*ptr < *fin){
+		if (!strcmp(str, (char*)(cont->get(cont, ptr)))){
+			match = 1;
+			break;
+		}
+		cont->next(cont, ptr);
+		if (!(*ptr))
+			*ptr = *fin; 
+	}
+	return match;
 }
